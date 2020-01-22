5c1507fbae48b8c06d867ca07827c4bc1077256e
clagosuarez <55792889+clagosuarez@users.noreply.github.com>
2020-01-22 10:46:39 +0100

    Add files via upload

---
+++ Galo.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bol16;
+
+/**
+ *
+ * @author clagosuarez
+ */
+public abstract class Galo implements IPodeCantar{
+    @Override
+    public void cantar(){
+        System.out.println("ki kiri ki");
+    }
+}
